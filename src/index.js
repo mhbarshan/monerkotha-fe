0ddec8ac04ecd443a2cfdae2eb96ec1e5b054292
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2026-05-19 00:35:31 +0600

    fix cookies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
